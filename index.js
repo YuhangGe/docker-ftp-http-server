5b894e355d6fbfb89e82842049570044d4b7e1b3
Yuhang <abeyuhang@gmail.com>
2019-05-08 11:44:43 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const path = require('path');
 const address = require('address');
 const HOST = process.env.FTP_HOST || address.ip() || "127.0.0.1";
 const PORT = process.env.FTP_PORT || "21";
-const ftpServer = new FtpSvr(`ftp://${HOST}:${PORT}`, { });
+const ftpServer = new FtpSvr({
+	url: `ftp://${HOST}:${PORT}`,
+	pasv_url: HOST
+});
 
 ftpServer.on('login', (data, resolve, reject) => {
   if (data.username==='hansight' && data.password==='hansight.com') {
